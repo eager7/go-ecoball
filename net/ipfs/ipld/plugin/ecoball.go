ae3e8aab3d29bdc96228fd1b0a8e601cbf9c8015
laixiaobing <laixb@126.com>
2018-08-10 16:18:23 +0800

    new api/plugin for adding serialized block to IPFS

--- net/ipfs/ipld/plugin/ecoball.go
+++ net/ipfs/ipld/plugin/ecoball.go
@@ -14,47 +14,16 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ecoball. If not, see <http://www.gnu.org/licenses/>.
 
+// Plugins is exported list of plugins that will be loaded
 package main
 
 import (
-	"fmt"
-	"github.com/ipfs/go-ipfs/core/coredag"
 	"github.com/ipfs/go-ipfs/plugin"
-	"github.com/ecoball/go-ecoball/net/ipfs/ipld"
-	"gx/ipfs/QmYVNvtQkeZ6AKSwDrjQTs432QtL6umrrK41EBq3cu7iSP/go-cid"
-	"gx/ipfs/QmZtNq8dArGfnpCZfx2pUNY7UcjGhVp5qqwQ4hH6mpTMRQ/go-ipld-format"
 )
 
-// Plugins is exported list of plugins that will be loaded
 var Plugins = []plugin.Plugin{
-	&ecoballPlugin{},
-}
+	&ecoballRawPlugin{},
 
-type ecoballPlugin struct{}
-
-var _ plugin.PluginIPLD = (*ecoballPlugin)(nil)
-
-func (this *ecoballPlugin) Name() string {
-	return "ipld-ecoball"
-}
-
-func (this *ecoballPlugin) Version() string {
-	return "0.0.1"
-}
-
-func (this *ecoballPlugin) Init() error {
-	fmt.Println("ecoball ipld plugin init.")
-	return nil
-}
-
-func (this *ecoballPlugin) RegisterBlockDecoders(dec format.BlockDecoder) error {
-	fmt.Println("ecoball ipld plugin register decoders.")
-	dec.Register(cid.EcoballBlock, ipldecoball.DecodeBlock)
-	return nil
-}
-
-func (this *ecoballPlugin) RegisterInputEncParsers(iec coredag.InputEncParsers) error {
-	iec.AddParser("raw", "ecoball-block", ipldecoball.EcoballBlockRawInputParser)
-	iec.AddParser("json", "ecoball-block", ipldecoball.EcoballBlockJsonInputParser)
-	return nil
+	// this is plugin is not ready
+	//&ecoballPlugin{},
 }
