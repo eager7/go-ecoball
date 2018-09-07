0ee57ebf62ac74182c206848bfe872984140db1b
panchangtao <eager7@163.com>
2018-09-07 17:23:03 +0800

    add account info in receipt

--- core/types/receipt.go
+++ core/types/receipt.go
@@ -1,6 +1,9 @@
 package types
 
-import "github.com/ecoball/go-ecoball/common"
+import (
+	"github.com/ecoball/go-ecoball/common"
+	"github.com/ecoball/go-ecoball/core/state"
+)
 
 const VirtualBlockCpuLimit float64 = 200000000.0
 const VirtualBlockNetLimit float64 = 1048576000.0
@@ -8,6 +11,8 @@ const BlockCpuLimit float64 = 200000.0
 const BlockNetLimit float64 = 1048576.0
 
 type TransactionReceipt struct {
+	From   *state.Account
+	To     *state.Account
 	Hash   common.Hash
 	Cpu    float64
 	Net    float64
