d7f52b08231cb045ec8165bb92fd855749991ae3
ubuntu <1345021934@qq.com>
2018-09-11 20:59:41 -0700

    modify rand api

--- smartcontract/wasmservice/rand.go
+++ smartcontract/wasmservice/rand.go
@@ -3,7 +3,6 @@ package wasmservice
 import (
 	"github.com/ecoball/go-ecoball/vm/wasmvm/exec"
 	"math/rand"
-	"time"
 )
 
 /*
@@ -11,7 +10,17 @@ param : scale - the max value of rand number
 param : seed  - the seed of rand function
 */
 func(ws *WasmService)rand(proc *exec.Process,scale ,seed int32)int32{
-	rnd:=rand.New(rand.NewSource(time.Now().Unix()+int64(seed)))
-	rand_num:=rnd.Intn(int(scale))
+	var rand_num int
+	rand_obj := rand.New(rand.NewSource(int64(seed)))
+	if seed == 0 {
+		rand_obj.Seed(1)
+	}
+
+	if scale == 0 {
+		rand_num = rand_obj.Int()
+	} else {
+		rand_num = rand_obj.Intn(int(scale))
+	}
+
 	return int32(rand_num)
 }
