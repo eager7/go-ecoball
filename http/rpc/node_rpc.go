3628ef26c822bfe282a9775e779d86d04469f93b
张清泉 <441881b00fq.cdb@sina.cn>
2018-09-18 16:32:54 +0800

    get info modify

--- http/rpc/node_rpc.go
+++ http/rpc/node_rpc.go
@@ -115,9 +115,6 @@ func StartRPCServer() (err error) {
 	//add attach
 	httpServer.AddHandleFunc("attach", commands.Attach)
 
-	//query
-	httpServer.AddHandleFunc("query", commands.Query)
-
 	//set contract
 	httpServer.AddHandleFunc("setContract", commands.SetContract)
 
