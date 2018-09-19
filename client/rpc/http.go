ee7c3389e2435a58703069f3d63cdfab1ed20bc7
jatel <jatelmomo@hotmail.com>
2018-09-19 19:52:32 +0800

    modify http

---
+++ client/rpc/http.go
@@ -0,0 +1,153 @@
+// Copyright 2018 The go-ecoball Authors
+// This file is part of the go-ecoball.
+//
+// The go-ecoball is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The go-ecoball is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-ecoball. If not, see <http://www.gnu.org/licenses/>.
+
+package rpc
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"strings"
+
+	"github.com/ecoball/go-ecoball/client/common"
+)
+
+//new request
+func newRequest(method, resource, address string, body io.Reader) (req *http.Request, err error) {
+	url := address + resource
+
+	req, err = http.NewRequest(method, url, body)
+	return
+}
+
+//post raw data
+func postRawResponse(resource, address, data string) ([]byte, error) {
+	fmt.Println(data)
+	req, err := newRequest("POST", resource, address, strings.NewReader(data))
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	res, err := http.DefaultClient.Do(req)
+	if nil != err {
+		return nil, err
+	}
+	defer func() {
+		io.Copy(ioutil.Discard, res.Body)
+		res.Body.Close()
+	}()
+
+	if res.StatusCode < 200 || res.StatusCode > 299 {
+		return nil, common.ReadAPIError(res.Body)
+	}
+
+	if res.StatusCode == http.StatusNotFound {
+		return nil, errors.New("API call not recognized: " + resource)
+	}
+
+	if res.StatusCode == http.StatusNoContent {
+		// no reason to read the response
+		return []byte{}, nil
+	}
+	return ioutil.ReadAll(res.Body)
+}
+
+//post method
+func post(resource, address, data string, obj interface{}) error {
+	body, err := postRawResponse(resource, address, data)
+	if nil != err {
+		return err
+	}
+
+	if obj == nil {
+		return nil
+	}
+
+	// Decode response
+	buf := bytes.NewBuffer(body)
+	err = json.NewDecoder(buf).Decode(obj)
+	return err
+}
+
+//get raw data
+func getRawResponse(resource, address string) ([]byte, error) {
+	req, err := newRequest("GET", resource, address, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := http.DefaultClient.Do(req)
+	if nil != err {
+		return nil, err
+	}
+	defer func() {
+		io.Copy(ioutil.Discard, res.Body)
+		res.Body.Close()
+	}()
+
+	if res.StatusCode < 200 || res.StatusCode > 299 {
+		return nil, common.ReadAPIError(res.Body)
+	}
+
+	if res.StatusCode == http.StatusNotFound {
+		return nil, errors.New("API call not recognized: " + resource)
+	}
+
+	if res.StatusCode == http.StatusNoContent {
+		// no reason to read the response
+		return []byte{}, nil
+	}
+	return ioutil.ReadAll(res.Body)
+}
+
+//get method
+func get(resource, address string, obj interface{}) error {
+	data, err := getRawResponse(resource, address)
+	if err != nil {
+		return err
+	}
+	if obj == nil {
+		// No need to decode response
+		return nil
+	}
+
+	// Decode response
+	buf := bytes.NewBuffer(data)
+	err = json.NewDecoder(buf).Decode(obj)
+	return err
+}
+
+//post
+func NodePost(resource, data string, obj interface{}) error {
+	return post(resource, common.RpcAddress(), data, obj)
+}
+
+func WalletPost(resource, data string, obj interface{}) error {
+	return post(resource, common.WalletRpcAddress(), data, obj)
+}
+
+//get
+func NodeGet(resource string, obj interface{}) error {
+	return get(resource, common.RpcAddress(), obj)
+}
+
+func WalletGet(resource string, obj interface{}) error {
+	return get(resource, common.WalletRpcAddress(), obj)
+}
---
+++ walletserver/http/http.go
@@ -0,0 +1,174 @@
+// Copyright 2018 The go-ecoball Authors
+// This file is part of the go-ecoball.
+//
+// The go-ecoball is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The go-ecoball is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-ecoball. If not, see <http://www.gnu.org/licenses/>.
+
+package http
+
+import (
+	"net/http"
+	"strings"
+
+	inner "github.com/ecoball/go-ecoball/common"
+	"github.com/ecoball/go-ecoball/common/config"
+	"github.com/ecoball/go-ecoball/walletserver/wallet"
+	"github.com/gin-gonic/gin"
+)
+
+func StartHttpServer() (err error) {
+	//get router instance
+	router := gin.Default()
+
+	//register handle
+	router.GET("/wallet/attach", attach)
+	router.POST("/wallet/create", createWallet)
+	router.POST("/wallet/createKey", createKey)
+	router.POST("/wallet/openWallet", openWallet)
+	router.POST("/wallet/lockWallet", lockWallet)
+	router.POST("/wallet/unlockWallet", unlockWallet)
+	router.POST("/wallet/importKey", importKey)
+	router.POST("/wallet/removeKey", removeKey)
+	router.POST("/wallet/listKey", listKey)
+	router.GET("/wallet/listWallets", listWallets)
+	router.GET("/wallet/getPublicKeys", getPublicKeys)
+	router.POST("/wallet/signTransaction", signTransaction)
+
+	http.ListenAndServe(":"+config.WalletHttpPort, router)
+	return nil
+}
+
+func attach(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"result": "success"})
+}
+
+func createWallet(c *gin.Context) {
+	name := c.PostForm("name")
+	password := c.PostForm("password")
+	if err := wallet.Create(name, []byte(password)); nil != err {
+		c.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+	}
+	c.JSON(http.StatusOK, gin.H{"result": "success"})
+}
+
+func createKey(c *gin.Context) {
+	name := c.PostForm("name")
+	pub, pri, err := wallet.CreateKey(name)
+	if nil != err {
+		c.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+	}
+	c.JSON(http.StatusOK, gin.H{"result": "publickey:" + inner.ToHex(pub) + "\n" + "privatekey:" + inner.ToHex(pri)})
+}
+
+func openWallet(c *gin.Context) {
+	name := c.PostForm("name")
+	password := c.PostForm("password")
+	if err := wallet.Open(name, []byte(password)); nil != err {
+		c.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+	}
+	c.JSON(http.StatusOK, gin.H{"result": "success"})
+}
+
+func lockWallet(c *gin.Context) {
+	name := c.PostForm("name")
+	if err := wallet.Lock(name); nil != err {
+		c.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+	}
+	c.JSON(http.StatusOK, gin.H{"result": "success"})
+}
+
+func unlockWallet(c *gin.Context) {
+	name := c.PostForm("name")
+	password := c.PostForm("password")
+	if err := wallet.Unlock(name, []byte(password)); nil != err {
+		c.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+	}
+	c.JSON(http.StatusOK, gin.H{"result": "success"})
+}
+
+func importKey(c *gin.Context) {
+	name := c.PostForm("name")
+	privateKey := c.PostForm("privateKey")
+	publickey, err := wallet.ImportKey(name, privateKey)
+	if nil != err {
+		c.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+	}
+	c.JSON(http.StatusOK, gin.H{"result": "publickey:" + inner.ToHex(publickey)})
+}
+
+func removeKey(c *gin.Context) {
+	name := c.PostForm("name")
+	password := c.PostForm("password")
+	publickey := c.PostForm("publickey")
+	err := wallet.RemoveKey(name, []byte(password), publickey)
+	if nil != err {
+		c.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+	}
+	c.JSON(http.StatusOK, gin.H{"result": "success"})
+}
+
+func listKey(c *gin.Context) {
+	name := c.PostForm("name")
+	password := c.PostForm("password")
+	accounts, err := wallet.ListKeys(name, []byte(password))
+	if nil != err {
+		c.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+	}
+	var key_str string
+	for k, v := range accounts {
+		key_str += "publickey:" + k + "\n" + "privatekey:" + v
+		key_str += "\n"
+	}
+	key_str = strings.TrimSuffix(key_str, "\n")
+	c.JSON(http.StatusOK, gin.H{"result": key_str})
+
+}
+
+func listWallets(c *gin.Context) {
+	wallets, err := wallet.List_wallets()
+	if nil != err {
+		c.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+	}
+	var walletsList string
+	for _, k := range wallets {
+		walletsList += k
+		walletsList += "\n"
+	}
+	walletsList = strings.TrimSuffix(walletsList, "\n")
+	c.JSON(http.StatusOK, gin.H{"result": walletsList})
+}
+
+func getPublicKeys(c *gin.Context) {
+	data, err := wallet.GetPublicKeys()
+	if nil != err {
+		c.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+	}
+	var publickeys string
+	for _, k := range data {
+		publickeys += k
+		publickeys += "\n"
+	}
+	publickeys = strings.TrimSuffix(publickeys, "\n")
+	c.JSON(http.StatusOK, gin.H{"result": publickeys})
+}
+
+func signTransaction(c *gin.Context) {
+	keys := c.PostForm("keys")
+	data := c.PostForm("data")
+	key := strings.Split(keys, "\n")
+	signData, err := wallet.SignTransaction(inner.FromHex(data), key)
+	if nil != err {
+		c.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+	}
+	c.JSON(http.StatusOK, gin.H{"result": inner.ToHex(signData)})
+}
