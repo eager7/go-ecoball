fd6121a88ab5bbea47ed0fa60fce319118d5d9b0
hucheng <hucheng007@163.com>
2018-09-29 00:27:21 -0700

    dsnget

--- client/commands/dsnstorage.go
+++ client/commands/dsnstorage.go
@@ -6,19 +6,19 @@ import (
    "github.com/urfave/cli"
   // "github.com/ecoball/go-ecoball/dsn"
    "github.com/ecoball/go-ecoball/client/rpc"
+
 )
 var (
 	DsnStorageCommands = cli.Command{
 		Name:     "dsnstorage",
-		Usage:    "operations for query state",
-		Category: "Get",
+		Usage:    "Distributed storage  interaction",
+		Category: "dsnstorage",
 		Subcommands: []cli.Command{
 			{
 				Name:   "dsnadd",
 				Usage:  "dsnadd file",
 				Action: dsnAddFile,
 				Flags: []cli.Flag{
-				
 					cli.StringFlag{
 						Name:  "add_-1, 1",
 						Usage: "add -1",
@@ -35,14 +35,25 @@ var (
 			},
 		},
 		
-		
 	}
+	
 )
 func dsnAddFile(ctx *cli.Context) error {
 
-	
+	var resp map[string]interface{}
+	var err error
 	//dsn.AddFile(os.Args[3],0)
-	resp, err := rpc.NodeCall("DsnAddFile", []interface{}{os.Args[0],os.Args[1],os.Args[2],os.Args[3],os.Args[4]})
+	if len(os.Args) <= 3  {
+		fmt.Println("error error need more than three Args")
+	}
+
+	if len(os.Args) == 4{
+		resp, err = rpc.NodeCall("DsnAddFile", []interface{}{os.Args[0],os.Args[1],os.Args[2],os.Args[3],"-1"})
+		fmt.Println("only 4 args, dsnadd file use default value -1")
+	}else if len(os.Args) == 5{
+		resp, err = rpc.NodeCall("DsnAddFile", []interface{}{os.Args[0],os.Args[1],os.Args[2],os.Args[3],os.Args[4]})
+	}
+	
 	if nil != resp["result"] {
 		switch resp["result"].(type) {
 		case string:
@@ -58,8 +69,15 @@ func dsnAddFile(ctx *cli.Context) error {
 
 func dsnGetFile(ctx *cli.Context) error {
 	
-	//dsn.AddFile(os.Args[3],0)
-	resp, err := rpc.NodeCall("DsnCatFile", []interface{}{os.Args[0],os.Args[1],os.Args[2],os.Args[3]})
+	var resp map[string]interface{}
+	var err error
+
+	if len(os.Args) == 4{
+		resp, err = rpc.NodeCall("DsnCatFile", []interface{}{os.Args[0],os.Args[1],os.Args[2],os.Args[3]})
+	}else{
+		fmt.Println("only input 4 args")
+	}
+
 	if nil != resp["result"] {
 		
 		switch resp["result"].(type) {
