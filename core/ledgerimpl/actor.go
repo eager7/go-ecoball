42ba4b046cd6ff416cc00a87ac85a9920d98ea98
panchangtao <eager7@163.com>
2018-10-19 14:47:04 +0800

    modified save block msg

--- core/ledgerimpl/actor.go
+++ core/ledgerimpl/actor.go
@@ -72,19 +72,19 @@ func (l *LedActor) Receive(ctx actor.Context) {
 		}
 		end := time.Now().UnixNano()
 		log.Info("save block["+msg.ChainID.HexString()+"block hash:"+msg.Hash.HexString()+"]:", (end-begin)/1000, "us")
-	case message.BlockMessage:
-		chain, ok := l.ledger.ChainTxs[msg.Block.GetChainID()]
+	case shard.BlockInterface:
+		chain, ok := l.ledger.ChainTxs[msg.GetChainID()]
 		if !ok {
-			log.Error(fmt.Sprintf("the chain:%s is not existed", msg.Block.GetChainID().HexString()))
+			log.Error(fmt.Sprintf("the chain:%s is not existed", msg.GetChainID().HexString()))
 			return
 		}
 		begin := time.Now().UnixNano()
-		if err := chain.SaveShardBlock(0, msg.Block); err != nil {
-			log.Error("save block["+msg.Block.GetChainID().HexString()+"] error:", err)
+		if err := chain.SaveShardBlock(0, msg); err != nil {
+			log.Error("save block["+msg.GetChainID().HexString()+"] error:", err)
 			break
 		}
 		end := time.Now().UnixNano()
-		log.Info("save block["+msg.Block.GetChainID().HexString()+"]:", (end-begin)/1000, "us")
+		log.Info("save block["+msg.GetChainID().HexString()+"]:", (end-begin)/1000, "us")
 	case *dpos.DposBlock:
 		//TODO
 		if err := event.Send(event.ActorLedger, event.ActorTxPool, msg.Block); err != nil {
