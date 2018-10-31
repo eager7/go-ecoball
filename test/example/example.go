90e7b4e58317cd183b14cb4d1c098edd53bae2c6
panchangtao <eager7@163.com>
2018-10-31 14:28:05 +0800

    compelete shard sync code

--- test/example/example.go
+++ test/example/example.go
@@ -1308,7 +1308,7 @@ func TransferExample() {
 	time.Sleep(time.Second * 15)
 	root := common.NameToIndex("root")
 	tester := common.NameToIndex("tester")
-	for i := 0; i < 0; i ++ {
+	for i := 0; i < 10; i ++ {
 		transfer, err := types.NewTransfer(root, tester, config.ChainHash, "active", new(big.Int).SetUint64(5), 101, time.Now().UnixNano())
 		errors.CheckErrorPanic(err)
 		transfer.SetSignature(&config.Root)
