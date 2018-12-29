357e64616b7e3c077f8a4f2604584ae5e2618d7e
panchangtao <eager7@163.com>
2018-12-29 09:51:11 +0800

    添加自动测试程序

---
+++ test/debug/test_net.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"os/exec"
+	"github.com/ecoball/go-ecoball/common/elog"
+	"github.com/ecoball/go-ecoball/common/errors"
+	"strings"
+	"os"
+	"time"
+)
+var log = elog.Log
+
+func main() {
+	i := 0
+	for {
+		i++
+		log.Debug("start test program:", i)
+		out, err := runCmd("./run.sh.bak")
+		errors.CheckErrorPanic(err)
+		time.Sleep(time.Second*15)
+		out, err = runCmd("find ecoball_log/ -name ecoball.log | xargs grep 123456789")
+		log.Notice(out)
+		out, err = runCmd("find ecoball_log/ -name ecoball.log | xargs grep ERROR")
+		errors.CheckErrorPanic(err)
+		log.Info(out)
+		if strings.Contains(out, "connection reset") {
+			os.Exit(0)
+		}
+	}
+}
+
+
+func runCmd(shell string) (string, error) {
+	cmd := exec.Command("/bin/bash", "-c", shell)
+	out, err := cmd.Output()
+	if err != nil {
+		log.Warn("exec ", cmd.Args, "failed, ", err.Error())
+		log.Warn(string(out))
+		return "", err
+	}
+	return string(out), err
+}
