ad1addb785f9da8bed90b9dbed6288460c1e91d4
panchangtao <eager7@163.com>
2019-01-04 10:29:18 +0800

    生成节点ID

--- test/rsakeygen/main.go
+++ test/rsakeygen/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	ic "gx/ipfs/Qme1knMqwt1hKZbc1BmQFmnm9f36nyQGwXxPGVpVJ9rMK5/go-libp2p-crypto"
+	"github.com/ecoball/go-ecoball/lib-p2p/address"
 )
 
 const nBitsForKeypairDefault  =  1024
@@ -21,5 +22,13 @@ func main() {
 	pubKey := privKey.GetPublic()
 	key, _ := ic.MarshalPublicKey(pubKey)
 	fmt.Println("Public  Key:", ic.ConfigEncodeKey(key))
+
+	b, _ := pubKey.Bytes()
+	pub := ic.ConfigEncodeKey(b)
+	id, err := address.IdFromPublicKey(pub)
+	if err != nil {
+		return
+	}
+	fmt.Println("Id Key:",id.Pretty())
 }
 
