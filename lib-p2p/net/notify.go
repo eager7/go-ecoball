54e216a4b15e3c7c24a2c7a12979e4ddca5ff33c
panchangtao <eager7@163.com>
2019-01-09 15:42:00 +0800

    format code

--- lib-p2p/net/notify.go
+++ lib-p2p/net/notify.go
@@ -5,8 +5,8 @@ import (
 	"gx/ipfs/QmYmsdtJ3HsodkePE3eU3TsCaP2YvPZJ4LoXnNkDE5Tpt7/go-multiaddr"
 )
 
-func (i *Instance) Listen(n net.Network, a multiaddr.Multiaddr)      { log.Debug("Listen") }
-func (i *Instance) ListenClose(n net.Network, a multiaddr.Multiaddr) { log.Debug("ListenClose") }
+func (i *Instance) Listen(n net.Network, a multiaddr.Multiaddr)      { log.Debug("Listen...") }
+func (i *Instance) ListenClose(n net.Network, a multiaddr.Multiaddr) { log.Debug("ListenClose...") }
 func (i *Instance) Connected(n net.Network, v net.Conn) {
 	log.Debug("Connected ID:", v.RemotePeer().Pretty(), "Addr:", n.Peerstore().Addrs(v.RemotePeer()))
 }
@@ -15,10 +15,8 @@ func (i *Instance) Disconnected(n net.Network, v net.Conn) {
 
 }
 func (i *Instance) OpenedStream(n net.Network, v net.Stream) {
-	//id := v.Conn().RemotePeer()
-	//addresses :=  n.Peerstore().Addrs(v.Conn().RemotePeer())
-	//log.Debug("OpenedStream", id.Pretty(), addresses, v.Conn().RemoteMultiaddr(), v)
+	log.Debug("OpenedStream", v.Conn().RemotePeer().Pretty(), v.Conn().RemoteMultiaddr())
 }
 func (i *Instance) ClosedStream(n net.Network, v net.Stream) {
-	//log.Debug("ClosedStream", n.Peerstore().Addrs(v.Conn().RemotePeer()), v)
+	log.Debug("ClosedStream", v.Conn().RemotePeer().Pretty(), n.Peerstore().Addrs(v.Conn().RemotePeer()), v)
 }
